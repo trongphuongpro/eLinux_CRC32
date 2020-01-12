0620127207884b2ca5f9becc86b8865a55c24692
ntppro <mr.trongphuongpro@gmail.com>
2020-01-12 13:24:54 +0300

    Small changes

--- test/test.c
+++ test/test.c
@@ -6,7 +6,7 @@
 int main() {
 	char *data[] = {"codelungtung.com", "trongphuongpro"};
 
-	crc32_t crc = crc32_concat(crc32_compute(data[0], strlen(data[0])), data[1], strlen(data[1]));
+	crc32_t crc = crc32_compute(data[0], strlen(data[0]));
 
 	printf("CRC32: %#x\n", crc);
 }
